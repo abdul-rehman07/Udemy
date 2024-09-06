16266ac9cd41360047140eadf0cdae15b078b884
abdul-rehman07 <abdulrehmankiyani7@gmail.com>
2024-09-06 17:24:42 +0500

    making components

--- propsreact/src/App.jsx
+++ propsreact/src/App.jsx
@@ -1,26 +1,15 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import CoreConcept from "./components/CoreConcept.jsx";
 import "./App.css";
-import componentsImg from "./assets/react.svg";
 import { CORE_CONCEPTS } from "./data.js";
 
-function CoreConcept({image,title,description}) {
-  return (
-    <li>
-      <img src={image} alt="..." />
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </li>
-  );
-}
+
 
 function App() {
   return (
     <>
       <main>
         <section id="core-concepts">
-          <h2 >Core concept</h2>
+          <h2 class="h2-class">Core concept</h2>
           <ul>
             <CoreConcept
             {...CORE_CONCEPTS[0]}
---
+++ propsreact/src/components/CoreConcept.jsx
@@ -0,0 +1,11 @@
+
+
+export default function CoreConcept({image,title,description}) {
+    return (
+      <li>
+        <img src={image} alt="..." />
+        <h2>{title}</h2>
+        <p>{description}</p>
+      </li>
+    );
+  }
